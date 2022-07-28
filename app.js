0443eb5186baef206865433abded76f031918496
syed muhammad bilal <102545337+syedmuhammadbilalshakeel@users.noreply.github.com>
2022-07-28 22:05:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function stop() {
+    var stopImg = document.getElementById("stop-images");
+    stopImg.src = "./1.jpeg";
+}
+function getReady() {
+    var stopImg = document.getElementById("stop-images");
+    stopImg.src = "./2.jpeg";
+}
+function go() {
+    var stopImg = document.getElementById("stop-images");
+    stopImg.src = "./3.jpeg";
+}
